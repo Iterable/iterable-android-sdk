11cfbe944e02b68042feb4c7a8ecd9661c3bf477
David Truong <dt@iterable.com>
2016-04-26 17:13:36 -0700

    Updated comments and added validation for the extra data passed in.

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableReceiver.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableReceiver.java
@@ -13,24 +13,33 @@ import org.json.JSONObject;
 
 /**
  * Created by davidtruong on 4/6/16.
+ *
+ * The IterableReceiver should be used to handle broadcasts to track push opens.
+ * The sending intent should use the action: IterableConstants.NOTIF_OPENED
+ * Additionally include the extra data passed down from GCM receive intent.
  */
 public class IterableReceiver extends BroadcastReceiver {
     static final String TAG = "IterableReceiver";
 
     private static final String INTENT_ACTION_GCM_RECEIVE = "com.google.android.c2dm.intent.RECEIVE";
 
+    /**
+     * IterableReceiver.onReceive
+     * @param context
+     * @param intent
+     */
     @CallSuper
     public void onReceive(Context context, Intent intent) {
         String intentAction = intent.getAction();
-        //TODO: should we handle all of the intent action here to queue up different
-        //use a switch statement here for each type of notif action
+        //TODO: should we handle all of the intent action here?
+            //use a switch statement here for each type of notif action
         if (intentAction.equals(INTENT_ACTION_GCM_RECEIVE)){
 
         } else if (intentAction.equals(IterableConstants.NOTIF_OPENED)){
-            Log.i(TAG, "notif_opened");
-            //TODO: Process the intent data and send to the Iterable Server
+            Log.i(TAG, "track ");
+
             Bundle extras = intent.getExtras();
-            if (extras != null && !extras.isEmpty())
+            if (extras != null && !extras.isEmpty() && extras.containsKey("itbl"))
             {
                 //TODO: data validation
                 String iterableData = extras.getString("itbl");
@@ -44,7 +53,7 @@ public class IterableReceiver extends BroadcastReceiver {
                     e.printStackTrace();
                 }
 
-                //TODO: pass in additional requestJson
+                //TODO: pass in additional requestJson from the system device
                 JSONObject requestJSON = new JSONObject();
 
                 //TODO: check that the sharedInstance exists before using it.
