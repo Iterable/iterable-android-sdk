126d68a5e441769027d3761a1bda98eb1a00c8bf
David Truong <dt@iterable.com>
2017-03-22 15:11:53 -0700

    misc style fixes

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableFirebaseMessagingService.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableFirebaseMessagingService.java
@@ -1,12 +1,8 @@
 package com.iterable.iterableapi;
 
-
-import android.util.Log;
-
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
-import java.util.Map;
 
 /**
  * Created by David Truong dt@iterable.com.
--- iterableapi/src/main/java/com/iterable/iterableapi/IterablePushReceiver.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterablePushReceiver.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.util.Log;
 
 /**
  *
