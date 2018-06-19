38ef84360978ecc76753815887bac6320e3bd188
Victor Babenko <victor@iterable.com>
2018-06-18 18:41:39 -0700

    Add a test for legacy payload

--- iterableapi/src/test/java/com/iterable/iterableapi/unit/IterableTestUtils.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/unit/IterableTestUtils.java
@@ -1,13 +1,18 @@
 package com.iterable.iterableapi.unit;
 
+import android.os.Bundle;
+
 import com.iterable.iterableapi.IterableApi;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.robolectric.RuntimeEnvironment;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 
 public class IterableTestUtils {
     public static void createIterableApi() {
@@ -27,5 +32,20 @@ public class IterableTestUtils {
 
         inputStream.close();
         return stringBuilder.toString();
+    }
+
+    public static Bundle getBundleFromJsonResource(String fileName) throws IOException, JSONException {
+        String jsonString = getResourceString(fileName);
+        return jsonToBundle(new JSONObject(jsonString));
+    }
+
+    public static Bundle jsonToBundle(JSONObject jsonObject) throws JSONException {
+        Bundle bundle = new Bundle();
+        for(Iterator<String> iterator = jsonObject.keys(); iterator.hasNext(); ){
+            String key = iterator.next();
+            Object value = jsonObject.get(key);
+            bundle.putString(key, value.toString());
+        }
+        return bundle;
     }
 }
