a2849f7e7884b5fb87c65b6066c5376326ba0529
Victor Babenko <victor@iterable.com>
2019-06-17 17:32:57 -0700

    Fix in-app persistency
    Also, trigger in-app queue update on app launch

--- iterableapi/src/test/java/com/iterable/iterableapi/IterableInAppFileStorageTest.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableInAppFileStorageTest.java
@@ -39,4 +39,29 @@ public class IterableInAppFileStorageTest {
         storage = new IterableInAppFileStorage(RuntimeEnvironment.application);
         assertEquals(0, storage.getMessages().size());
     }
+
+    @Test
+    public void testInAppImplicitSave() throws Exception {
+        IterableInAppFileStorage storage = new IterableInAppFileStorage(RuntimeEnvironment.application);
+        IterableInAppMessage testInAppMessage = InAppTestUtils.getTestInAppMessage();
+        storage.addMessage(testInAppMessage);
+
+        storage = new IterableInAppFileStorage(RuntimeEnvironment.application);
+        assertEquals(1, storage.getMessages().size());
+        testInAppMessage = storage.getMessages().get(0);
+        testInAppMessage.setProcessed(true);
+        testInAppMessage.setConsumed(true);
+
+        storage = new IterableInAppFileStorage(RuntimeEnvironment.application);
+        assertEquals(1, storage.getMessages().size());
+        testInAppMessage = storage.getMessages().get(0);
+        JSONAssert.assertEquals(testInAppMessage.toJSONObject(), storage.getMessages().get(0).toJSONObject(), JSONCompareMode.STRICT);
+        assertTrue(storage.getMessages().get(0).isProcessed());
+        assertTrue(storage.getMessages().get(0).isConsumed());
+
+        storage.removeMessage(storage.getMessage(testInAppMessage.getMessageId()));
+        assertEquals(0, storage.getMessages().size());
+        storage = new IterableInAppFileStorage(RuntimeEnvironment.application);
+        assertEquals(0, storage.getMessages().size());
+    }
 }
