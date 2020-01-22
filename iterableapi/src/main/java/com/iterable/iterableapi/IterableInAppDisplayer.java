29cb096928476395005d15d3a8a03f1f202479d1
Victor Babenko <victor@iterable.com>
2020-01-21 17:10:18 -0800

    WIP - migrate in-app rendering to DialogFragment

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableInAppDisplayer.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableInAppDisplayer.java
@@ -4,6 +4,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Rect;
+import android.support.v4.app.FragmentActivity;
+
+import java.util.Random;
 
 class IterableInAppDisplayer {
 
@@ -17,17 +20,31 @@ class IterableInAppDisplayer {
         return IterableInAppHTMLNotification.getInstance() != null;
     }
 
+//    boolean isShowingInApp() {
+//        return IterableInAppFragmentHTMLNotification.getInstance() != null;
+//    }
+
     boolean showMessage(IterableInAppMessage message, IterableInAppLocation location, final IterableHelper.IterableUrlCallback clickCallback) {
         Activity currentActivity = activityMonitor.getCurrentActivity();
         // Prevent double display
         if (currentActivity != null) {
-            return IterableInAppDisplayer.showIterableNotificationHTML(currentActivity,
-                    message.getContent().html,
-                    message.getMessageId(),
-                    clickCallback,
-                    message.getContent().backgroundAlpha,
-                    message.getContent().padding,
-                    true, location);
+            if (new Random().nextBoolean()) {
+                return IterableInAppDisplayer.showIterableFragmentNotificationHTML(currentActivity,
+                        message.getContent().html,
+                        message.getMessageId(),
+                        clickCallback,
+                        message.getContent().backgroundAlpha,
+                        message.getContent().padding,
+                        true, location);
+            } else {
+                return IterableInAppDisplayer.showIterableNotificationHTML(currentActivity,
+                        message.getContent().html,
+                        message.getMessageId(),
+                        clickCallback,
+                        message.getContent().backgroundAlpha,
+                        message.getContent().padding,
+                        true, location);
+            }
         }
         return false;
     }
@@ -68,6 +85,50 @@ class IterableInAppDisplayer {
                 }
 
                 notification.show();
+                return true;
+            }
+        } else {
+            IterableLogger.w(IterableInAppManager.TAG, "To display in-app notifications, the context must be of an instance of: Activity");
+        }
+        return false;
+    }
+
+    /**
+     * Displays an html rendered InApp Notification
+     * @param context
+     * @param htmlString
+     * @param messageId
+     * @param clickCallback
+     * @param backgroundAlpha
+     * @param padding
+     */
+    static boolean showIterableFragmentNotificationHTML(Context context, String htmlString, String messageId, final IterableHelper.IterableUrlCallback clickCallback, double backgroundAlpha, Rect padding, boolean callbackOnCancel, IterableInAppLocation location) {
+        if (context instanceof FragmentActivity) {
+            FragmentActivity currentActivity = (FragmentActivity) context;
+            if (htmlString != null) {
+                if (IterableInAppFragmentHTMLNotification.getInstance() != null) {
+                    IterableLogger.w(IterableInAppManager.TAG, "Skipping the in-app notification: another notification is already being displayed");
+                    return false;
+                }
+
+                IterableInAppFragmentHTMLNotification notification = IterableInAppFragmentHTMLNotification.createInstance(context, htmlString);
+                notification.setTrackParams(messageId);
+                notification.setCallback(clickCallback);
+                notification.setBackgroundAlpha(backgroundAlpha);
+                notification.setPadding(padding);
+                //notification.setOwnerActivity(currentActivity);
+                notification.setLocation(location);
+
+                if (callbackOnCancel) {
+//                    notification.getDialog().setOnCancelListener(new DialogInterface.OnCancelListener() {
+//                        @Override
+//                        public void onCancel(DialogInterface dialog) {
+//                            clickCallback.execute(null);
+//                        }
+//                    });
+                }
+
+                notification.show(currentActivity.getSupportFragmentManager(), "iterable_in_app");
                 return true;
             }
         } else {
---
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableWebViewClient.java
@@ -0,0 +1,46 @@
+package com.iterable.iterableapi;
+
+import android.net.Uri;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * Custom webViewClient which handles url clicks
+ */
+class IterableWebViewClient extends WebViewClient {
+    static final String resizeScript = "javascript:ITBL.resize(document.body.getBoundingClientRect().height)";
+
+    HTMLNotificationCallbacks inAppHTMLNotification;
+
+    IterableWebViewClient(HTMLNotificationCallbacks inAppHTMLNotification) {
+        this.inAppHTMLNotification = inAppHTMLNotification;
+    }
+
+    /**
+     * Handles url clicks
+     * @param view
+     * @param url
+     * @return
+     */
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+        inAppHTMLNotification.onUrlClicked(url);
+        return true;
+    }
+
+    /**
+     * Resizes the view after the page has loaded
+     * @param view
+     * @param url
+     */
+    @Override
+    public void onPageFinished(WebView view, String url) {
+        inAppHTMLNotification.setLoaded(true);
+        view.loadUrl(resizeScript);
+    }
+
+    public interface HTMLNotificationCallbacks {
+        void onUrlClicked(String url);
+        void setLoaded(Boolean loaded);
+    }
+}
