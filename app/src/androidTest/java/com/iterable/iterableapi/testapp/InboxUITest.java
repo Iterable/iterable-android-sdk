d576be5507080763fa7052760575e4ee63b78e0b
Victor Babenko <victor@iterable.com>
2020-08-27 18:42:29 -0700

    Fix tests

--- app/src/androidTest/java/com/iterable/iterableapi/testapp/InboxUITest.java
+++ app/src/androidTest/java/com/iterable/iterableapi/testapp/InboxUITest.java
@@ -3,6 +3,7 @@ package com.iterable.iterableapi.testapp;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
 
+import com.iterable.iterableapi.IterableApi;
 import com.iterable.iterableapi.ui.R;
 import com.iterable.iterableapi.ui.inbox.IterableInboxActivity;
 
@@ -10,6 +11,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static androidx.test.core.app.ApplicationProvider.getApplicationContext;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -18,7 +20,13 @@ import static org.junit.Assert.assertNotNull;
 @RunWith(AndroidJUnit4.class)
 public class InboxUITest {
     @Rule
-    public ActivityTestRule rule = new ActivityTestRule<>(IterableInboxActivity.class);
+    public ActivityTestRule<IterableInboxActivity> rule = new ActivityTestRule<IterableInboxActivity>(IterableInboxActivity.class) {
+        @Override
+        protected void beforeActivityLaunched() {
+            super.beforeActivityLaunched();
+            IterableApi.initialize(getApplicationContext(), "apiKey");
+        }
+    };
 
     @Test
     public void basicTest() {
