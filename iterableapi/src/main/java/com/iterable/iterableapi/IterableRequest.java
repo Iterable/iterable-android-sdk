a421a559c0378eb79e1d5946bc23f64f6275b549
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2020-12-07 08:31:11 -0800

    [MOB-2150] - Request Serialization
    1. Added from JSON and toJSON to IterableApiRequest class.
    2. Added temporary task schedular code in offline request processor to see if it can form the request from the database

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableRequest.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableRequest.java
@@ -293,6 +293,8 @@ class IterableRequest extends AsyncTask<IterableApiRequest, Void, String> {
  *  Iterable Request object
  */
 class IterableApiRequest {
+    private static final String TAG = "IterableApiRequest";
+
     static final String GET = "GET";
     static final String POST = "POST";
 
@@ -337,5 +339,32 @@ class IterableApiRequest {
         this.requestType = requestType;
         this.authToken = authToken;
         this.legacyCallback = callback;
+    }
+
+    public JSONObject toJSONObject() throws JSONException {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("apiKey", this.apiKey);
+        jsonObject.put("endPoint", this.resourcePath);
+        jsonObject.put("auth", this.authToken);
+        jsonObject.put("requestType", this.requestType);
+        jsonObject.put("data", this.json);
+        return jsonObject;
+    }
+
+    static IterableApiRequest fromJSON(JSONObject jsonData, @Nullable IterableHelper.SuccessHandler onSuccess, @Nullable IterableHelper.FailureHandler onFailure) {
+        try {
+            String apikey = jsonData.getString("apiKey");
+            String resourcePath = jsonData.getString("endPoint");
+            String requestType = jsonData.getString("requestType");
+            String auth = "";
+            if(jsonData.has("auth")){
+                auth = jsonData.getString("auth");
+            }
+            JSONObject json = jsonData.getJSONObject("data");
+            return new IterableApiRequest(apikey,resourcePath, json, requestType, auth, onSuccess, onFailure );
+        } catch (JSONException e) {
+            IterableLogger.e(TAG, "Failed to create Iterable request from JSON");
+        }
+        return null;
     }
 }
--- iterableapi/src/main/java/com/iterable/iterableapi/OfflineRequestProcessor.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/OfflineRequestProcessor.java
@@ -3,17 +3,77 @@ package com.iterable.iterableapi;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import com.google.android.gms.tasks.Task;
+
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class OfflineRequestProcessor implements RequestProcessor {
 
+
     @Override
     public void processGetRequest(@Nullable String apiKey, @NonNull String resourcePath, @NonNull JSONObject json, String authToken, @Nullable IterableHelper.IterableActionHandler onCallback) {
-
+        //Invoke taskScheduler and add task to database
     }
 
     @Override
     public void processPostRequest(@Nullable String apiKey, @NonNull String resourcePath, @NonNull JSONObject json, String authToken, @Nullable IterableHelper.SuccessHandler onSuccess, @Nullable IterableHelper.FailureHandler onFailure) {
+        IterableApiRequest request = new IterableApiRequest(apiKey, resourcePath, json, IterableApiRequest.POST, authToken, onSuccess, onFailure);
+        TaskScheduler taskScheduler = new TaskScheduler();
+        taskScheduler.scheduleTask(request, onSuccess, onFailure);
+    }
+}
 
+
+class TaskScheduler {
+    private final String TAG = "RequestProcessor";
+
+    static HashMap<String, IterableHelper.SuccessHandler> successCallbackMap = new HashMap<>();
+    static HashMap<String, IterableHelper.FailureHandler> failureCallbackMap = new HashMap<>();
+
+    void scheduleTask(IterableApiRequest request, @Nullable IterableHelper.SuccessHandler onSuccess, @Nullable IterableHelper.FailureHandler onFailure) {
+        IterableTaskManager taskManager = IterableTaskManager.sharedInstance(IterableApi.getInstance().getMainActivityContext());
+        JSONObject iterableRequestOffline = null;
+        try {
+            iterableRequestOffline = request.toJSONObject();
+        } catch (JSONException e) {
+            IterableLogger.e(TAG, "Failed serializating the request for offline execution. Attempting to request the request now...");
+            new IterableRequest().execute(request);
+            return;
+        }
+
+        String taskId = taskManager.createTask(request.resourcePath, IterableTaskType.API, iterableRequestOffline.toString());
+
+        successCallbackMap.put(taskId, onSuccess);
+        failureCallbackMap.put(taskId,onFailure);
+
+        processTask();
+    }
+
+    //Temporary function to convert database offline task to ITerableReuqest and execute.
+    void processTask() {
+        IterableTaskManager taskManager = IterableTaskManager.sharedInstance(IterableApi.getInstance().getMainActivityContext());
+        ArrayList<String> taskIds = taskManager.getAllTaskIds();
+        for (String id : taskIds
+        ) {
+            try {
+                IterableApiRequest request = makeRequestFromTask(taskManager.getTask(id));
+                new IterableRequest().execute(request);
+                taskManager.deleteTask(id);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    IterableApiRequest makeRequestFromTask(IterableTask task) throws JSONException {
+        IterableHelper.SuccessHandler onSuccess = successCallbackMap.get(task.id);
+        IterableHelper.FailureHandler onFailure = failureCallbackMap.get(task.id);
+        successCallbackMap.remove(task.id);
+        failureCallbackMap.remove(task.id);
+        return IterableApiRequest.fromJSON(new JSONObject(task.data), onSuccess, onFailure);
     }
 }
