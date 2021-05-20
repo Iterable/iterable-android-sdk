0fe43c1817faad2aae76cc9aa26a10ba15bec75d
Jay Kim <jay@nullavenue.com>
2021-05-19 18:11:05 -0700

    stub unit test

--- iterableapi/src/test/java/com/iterable/iterableapi/HealthMonitorTests.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/HealthMonitorTests.java
@@ -2,9 +2,32 @@ package com.iterable.iterableapi;
 
 import com.iterable.iterableapi.unit.TestRunner;
 
+import org.json.JSONObject;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 
 @RunWith(TestRunner.class)
 public class HealthMonitorTests {
-    
+    private IterableTaskStorage mockTaskStorage;
+    private IterableTaskRunner mockTaskRunner;
+    private TaskScheduler mockTaskScheduler;
+
+    @Before
+    public void setUp() {
+        mockTaskStorage = mock(IterableTaskStorage.class);
+        mockTaskRunner = mock(IterableTaskRunner.class);
+        mockTaskScheduler = mock(TaskScheduler.class);
+    }
+
+    @Test
+    public void testUseOfflineProcessorByDefault() throws Exception {
+        IterableApiRequest request = new IterableApiRequest("apiKey", "api/test", new JSONObject(), "POST", null, null, null);
+        
+        verifyNoInteractions(mockTaskScheduler);
+    }
 }
