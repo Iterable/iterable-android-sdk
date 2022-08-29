4caf9d620d8e9e51ea51e5d0fd4085b4c323ad64
Jay Kim <jay@nullavenue.com>
2022-08-29 11:23:03 -0700

    remove unused import

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
@@ -1,7 +1,6 @@
 package com.iterable.iterableapi;
 
 import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
