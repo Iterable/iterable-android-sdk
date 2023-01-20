31ca2e91922ab200d19ffbb9cca0500bc2f8f7c5
Evan Greer <evan.greer@iterable.com>
2023-01-20 09:51:55 -0700

    stashed changes

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewActivity.java
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewActivity.java
@@ -1,4 +1,15 @@
 package com.iterable.iterableapi.ui.flex;
 
-public class IterableFlexViewActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IterableFlexViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate() {
+        setContentView()
+
+    }
 }
