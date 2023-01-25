2a9074c9ba6bf904835df55fb0d278040b58fdc8
Evan Greer <evan.greer@iterable.com>
2023-01-25 12:10:48 -0700

    converts IterableFlexViewFragment to Kotlin

---
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewFragment.kt
@@ -0,0 +1,19 @@
+package com.iterable.iterableapi.ui.flex
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.iterable.iterableapi.ui.R
+
+class IterableFlexViewFragment : Fragment() {
+    //    var flexMessages =
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.iterable_flex_view_fragment, container, false)
+    }
+}
