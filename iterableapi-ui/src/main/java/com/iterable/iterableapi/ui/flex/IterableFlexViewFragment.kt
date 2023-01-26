3fe6e0fdada93fcd76ae718dd5c0b6e24e08e67f
Evan Greer <evan.greer@iterable.com>
2023-01-26 09:10:49 -0700

    stashed changes

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewFragment.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewFragment.kt
@@ -4,16 +4,54 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.iterable.iterableapi.*
 import com.iterable.iterableapi.ui.R
 
 class IterableFlexViewFragment : Fragment() {
-    //    var flexMessages =
+    // example flex message data from a payload
+    private val flexMessageMetaData: IterableFlexMessageMetaData = IterableFlexMessageMetaData(
+        "doibjo4590340oidiobnw",
+        "mbn8489b7ehycy",
+        "noj9iyjthfvhs",
+        false
+    )
+
+    private val flexMessageButtons: List<IterableFlexMessageButton> = listOf(
+        IterableFlexMessageButton("reward-button", "REDEEM MEOW", "success")
+    )
+
+    private val flexMessageImages: List<IterableFlexMessageImage> = listOf(
+        IterableFlexMessageImage("coffee-image", "https://example-image-url.com/first-image")
+    )
+
+    private val flexMessageText: List<IterableFlexMessageText> = listOf(
+        IterableFlexMessageText("title", "CATS RULE!!!"),
+        IterableFlexMessageText("body", "GET FRESH COFFEE")
+    )
+
+    private val flexMessageElements: IterableFlexMessageElements = IterableFlexMessageElements(
+        "hero",
+        flexMessageButtons,
+        flexMessageImages,
+        flexMessageText
+    )
+
+    private var flexMessage: IterableFlexMessage = IterableFlexMessage(flexMessageMetaData, flexMessageElements,  "")
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.iterable_flex_view_fragment, container, false)
+        val view = inflater.inflate(R.layout.iterable_flex_view_fragment, container, false)
+        val flexMessageText: TextView  = view.findViewById<TextView>(R.id.body)
+        val flexMessageButton: Button = view.findViewById<Button>(R.id.flexMessageButton)
+
+        flexMessageText.text = flexMessage.elements.text.find(it)
+
+        return view
     }
 }
--- iterableapi/src/main/java/com/iterable/iterableapi/IterableFlexMessage.kt
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableFlexMessage.kt
@@ -1,15 +1,13 @@
 package com.iterable.iterableapi
 
-data class IterableFlexMessage(var metadata: IterableFlexMessageMetaData, var elements: IterableFlexMessageElements, var custom: IterableCustomPayload)
+data class IterableFlexMessage(var metadata: IterableFlexMessageMetaData, var elements: IterableFlexMessageElements, var custom: Any)
 
 data class IterableFlexMessageMetaData(var id: String, var placementId: String, var campaignId: String, var isProof: Boolean)
 
-data class IterableFlexMessageElements(var type: String, var buttons: List<FlexMessageButton>, var images: List<FlexMessageImage>, var text: List<FlexMessageText>)
+data class IterableFlexMessageElements(var type: String, var buttons: List<IterableFlexMessageButton>, var images: List<IterableFlexMessageImage>, var text: List<IterableFlexMessageText>)
 
-data class FlexMessageButton(var id: String, var title: String, var action: String)
+data class IterableFlexMessageButton(var id: String, var title: String, var action: String)
 
-data class FlexMessageImage(var id: String, var url: String)
+data class IterableFlexMessageImage(var id: String, var url: String)
 
-data class FlexMessageText(var title: String, var text: String)
-
-data class IterableCustomPayload(var key: Any)
+data class IterableFlexMessageText(var title: String, var text: String)
