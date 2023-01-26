748dda9ca0d17a76424d694eb0a32fc632885eed
Evan Greer <evan.greer@iterable.com>
2023-01-25 18:42:17 -0700

    updates data class for flex message

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexMessage.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexMessage.kt
@@ -2,16 +2,14 @@ package com.iterable.iterableapi.ui.flex
 
 data class IterableFlexMessage(var metadata: IterableFlexMessageMetaData, var elements: IterableFlexMessageElements, var custom: IterableCustomPayload)
 
-data class IterableFlexMessageMetaData(var id: String = "", var placementId: String = "", var campaignId: String = "", var isProof: Boolean)
+data class IterableFlexMessageMetaData(var id: String, var placementId: String, var campaignId: String, var isProof: Boolean)
 
-data class IterableFlexMessageElements(var type: String = "", var buttons: FlexMessageButtons, var images: FlexMessageImage, var text: FlexMessageText)
+data class IterableFlexMessageElements(var type: String, var buttons: List<FlexMessageButton>, var images: List<FlexMessageImage>, var text: List<FlexMessageText>)
 
-data class FlexMessageButtons(var buttonId: String = "", var button: FlexMessageButtonData)
+data class FlexMessageButton(var id: String, var title: String, var action: String)
 
-data class FlexMessageButtonData(var title: String = "", var action: String = "")
+data class FlexMessageImage(var id: String, var url: String)
 
-data class FlexMessageImage(var imageId: String = "", var imageUrl: String = "")
+data class FlexMessageText(var title: String, var text: String)
 
-data class FlexMessageText(var title: String = "", var body: String = "")
-
-data class IterableCustomPayload(var key: String = "", var value: String = "")
+data class IterableCustomPayload(var key: Any)
