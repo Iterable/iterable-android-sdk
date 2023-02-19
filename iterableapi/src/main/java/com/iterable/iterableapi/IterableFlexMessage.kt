615da3a705d01231225866e80d7193f6139899cf
Evan Greer <evan.greer@iterable.com>
2023-02-18 18:32:44 -0700

    updates data types for payload redesign

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableFlexMessage.kt
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableFlexMessage.kt
@@ -1,37 +1,34 @@
 package com.iterable.iterableapi
-
-class IterableFlexMessage(
-    var metadata: FlexMessageMetaData,
-    var elements: FlexMessageElements? = null,
-    var custom: Any? = null
-) {
-    class FlexMessageMetaData(
-        var id: String,
-        var placementId: String,
-        var campaignId: String? = null,
-        var isProof: Boolean? = false
-    )
-
-    class FlexMessageElements (
-        var type: String? = null,
-        var buttons: List<FlexMessageButton>? = null,
-        var images: List<FlexMessageImage>? = null,
-        var text: List<FlexMessageText>? = null
-    ) {
-        class FlexMessageButton(
-            var id: String,
-            var title: String? = null,
-            var action: String? = null
-        )
-
-        class FlexMessageImage(
-            var id: String,
-            var url: String? = null
-        )
-
-        class FlexMessageText(
-            var id: String,
-            var text: String? = null
-        )
-    }
-}
+data class IterableFlexMessage (
+    val metadata: FlexMessageMetadata,
+    val elements: FlexMessageElements? = null,
+    val payload: HashMap<Any, Any>? = null
+)
+data class FlexMessageMetadata (
+    val id: String,
+    val placementID: String,
+    val campaignID: String? = null,
+    val isProof: Boolean? = null
+)
+data class FlexMessageElements (
+    val title: String? = null,
+    val body: String? = null,
+    val mediaURL: String? = null,
+    val defaultAction: FlexMessageElementsDefaultAction? = null,
+    val buttons: List<FlexMessageElementsButton>? = null,
+    val text: List<FlexMessageElementsText>? = null
+)
+data class FlexMessageElementsButton (
+    val id: String,
+    val title: String? = null,
+    val action: String? = null
+)
+data class FlexMessageElementsDefaultAction (
+    val type: String,
+    val data: String
+)
+data class FlexMessageElementsText (
+    val id: String,
+    val text: String? = null,
+    val label: String? = null
+)
