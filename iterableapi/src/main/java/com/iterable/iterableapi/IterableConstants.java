1f4d1c0616d6d5c633b6246fd9bf4e8ccc1c0c0d
Akshay Ayyanchira <ayyanchira.akshay@gmail.com>
2023-02-21 00:12:28 -0800

    [MOB-5664] - Revert channel name implementation
    Revert the code where the channel name was derived by reading value from Android Manifest. If not value is found, a default name "iterable channel" is provided.
    Now, with custom sound, if a soundURI exists from a provided custom sound, only then soundName is treated as a channel name. Otherwise, old way of handling the name still persists.

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableConstants.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableConstants.java
@@ -149,7 +149,7 @@ public final class IterableConstants {
     public static final String NOTIFICATION_ICON_NAME   = "iterable_notification_icon";
     public static final String NOTIFICAION_BADGING      = "iterable_notification_badging";
     public static final String NOTIFICATION_COLOR       = "iterable_notification_color";
-    public static final String NOTIFICATION_CHANNEL_NAME = "Default";
+    public static final String NOTIFICATION_CHANNEL_NAME = "iterable_notification_channel_name";
     public static final String DEFAULT_SOUND            = "default";
     public static final String SOUND_FOLDER_IDENTIFIER  = "raw";
     public static final String ANDROID_RESOURCE_PATH    = "android.resource://";
