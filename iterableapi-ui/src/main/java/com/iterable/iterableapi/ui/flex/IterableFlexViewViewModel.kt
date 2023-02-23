cd82a6e946dc88e3c9c520a4287ed092b94b11d9
Evan Greer <evan.greer@iterable.com>
2023-02-22 20:39:11 -0700

    adds serialization example

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewViewModel.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/flex/IterableFlexViewViewModel.kt
@@ -10,43 +10,48 @@ import com.iterable.iterableapi.*
 
 class IterableFlexViewViewModel : ViewModel() {
 
-    // example flex message data from a payload
-    private val flexMessageMetaData = FlexMessageMetadata(
-        "doibjo4590340oidiobnw",
-        "mbn8489b7ehycy",
-        "noj9iyjthfvhs",
-        false
-    )
-
-    private val flexMessageDefaultAction = FlexMessageElementsDefaultAction(
-        "someType", "someAction"
-    )
-
-    private val flexMessageButtons: List<FlexMessageElementsButton> = listOf(
-        FlexMessageElementsButton("reward-button", "REDEEM MEOW", "success")
-    )
-
-
-    private val flexMessageText: List<FlexMessageElementsText> = listOf(
-        FlexMessageElementsText("body", "CATS RULE!!!", "label")
-    )
-
-    private val flexMessageElements = FlexMessageElements(
-        "Iterable Coffee Shoppe",
-        "Get 15% OFF",
-        "http://placekitten.com/200/300",
-        flexMessageDefaultAction,
-        flexMessageButtons,
-        flexMessageText
-    )
-
-    val payload = JSONObject()
-
     private var _flexMessage = MutableLiveData<IterableFlexMessage>()
     val flexMessage: LiveData<IterableFlexMessage>
         get() = _flexMessage
 
     init {
-        _flexMessage.value = IterableFlexMessage(flexMessageMetaData, flexMessageElements, payload)
+
+        // example of a JSON payload being serialized
+        val flexMessageMetadata = FlexMessageMetadata(
+            "doibjo4590340oidiobnw",
+            "mbn8489b7ehycy",
+            "noj9iyjthfvhs",
+            false
+        )
+
+        val flexMessageDefaultAction = FlexMessageElementsDefaultAction(
+            "someType", "someAction"
+        )
+
+        val flexMessageButtons: List<FlexMessageElementsButton> = listOf(
+            FlexMessageElementsButton("reward-button", "REDEEM MEOW", "success")
+        )
+
+        val flexMessageText: List<FlexMessageElementsText> = listOf(
+            FlexMessageElementsText("body", "CATS RULE!!!", "label")
+        )
+
+        val flexMessageElements = FlexMessageElements(
+            "Iterable Coffee Shoppe",
+            "Get 15% OFF",
+            "http://placekitten.com/200/300",
+            flexMessageDefaultAction,
+            flexMessageButtons,
+            flexMessageText
+        )
+
+        val payload = JSONObject()
+
+        val flexMessageJson = JSONObject()
+        flexMessageJson.put("metadata", flexMessageMetadata.toJSONObject())
+        flexMessageJson.put("elements", flexMessageElements.toJSONObject())
+        flexMessageJson.put("payload", payload)
+
+        _flexMessage.value = IterableFlexMessage.fromJSONObject(flexMessageJson)
     }
 }
