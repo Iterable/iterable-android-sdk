57484fa3a7ecec3168967cb384a66b370cb6d45d
Evan Greer <evan.greer@iterable.com>
2023-02-25 16:58:43 -0700

    add demo embedded message to fragment:q

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedViewFragment.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedViewFragment.kt
@@ -4,15 +4,60 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.iterable.iterableapi.*
 import com.iterable.iterableapi.ui.R
+import org.json.JSONObject
 
 class IterableEmbeddedViewFragment : Fragment() {
+    // example flex message data from a payload
+    private val embeddedMessageMetaData = EmbeddedMessageMetadata(
+        "doibjo4590340oidiobnw",
+        "mbn8489b7ehycy",
+        "noj9iyjthfvhs",
+        false
+    )
+
+    private val embeddedMessageDefaultAction = EmbeddedMessageElementsDefaultAction(
+        "someType", "someAction"
+    )
+
+    private val embeddedMessageButtons: List<EmbeddedMessageElementsButton> = listOf(
+        EmbeddedMessageElementsButton("reward-button", "REDEEM MEOW", "success")
+    )
+
+
+    private val embeddedMessageText: List<EmbeddedMessageElementsText> = listOf(
+        EmbeddedMessageElementsText("body", "CATS RULE!!!", "label")
+    )
+
+    private val embeddedMessageElements = EmbeddedMessageElements(
+        "Iterable Coffee Shoppe",
+        "Get 15% OFF",
+        "http://placekitten.com/200/300",
+        embeddedMessageDefaultAction,
+        embeddedMessageButtons,
+        embeddedMessageText
+    )
+
+    val payload = JSONObject()
+
+    private var embeddedMessage: IterableEmbeddedMessage = IterableEmbeddedMessage(embeddedMessageMetaData, embeddedMessageElements, payload)
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.iterable_embedded_view_fragment, container, false)
+        var view = inflater.inflate(R.layout.iterable_embedded_view_fragment, container, false)
+
+        var flexMessageText = view.findViewById<TextView>(R.id.embeddedMessageBody)
+        var flexMessageButton = view.findViewById<Button>(R.id.embeddedMessageButton)
+
+        flexMessageText.text = embeddedMessage.elements?.text?.get(0)?.text
+        flexMessageButton.text = embeddedMessage.elements?.buttons?.get(0)?.title
+
+        return view
     }
 }
