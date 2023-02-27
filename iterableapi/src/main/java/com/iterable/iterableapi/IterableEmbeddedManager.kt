2f6ae52d48a6c193bb6170c6d5fc1a172c997bff
Evan Greer <evan.greer@iterable.com>
2023-02-27 16:06:31 -0700

    reads message json from file

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableEmbeddedManager.kt
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableEmbeddedManager.kt
@@ -1,49 +1,19 @@
 package com.iterable.iterableapi
 
 import org.json.JSONObject
+import java.io.File
 
 public class IterableEmbeddedManager {
     fun getMessages(): List<IterableEmbeddedMessage> {
-        // example of a JSON payload being serialized
-        val flexMessageMetadata = EmbeddedMessageMetadata(
-            "doibjo4590340oidiobnw",
-            "mbn8489b7ehycy",
-            "noj9iyjthfvhs",
-            false
-        )
-
-        val flexMessageDefaultAction = EmbeddedMessageElementsDefaultAction(
-            "someType", "someAction"
-        )
-
-        val flexMessageButtons: List<EmbeddedMessageElementsButton> = listOf(
-            EmbeddedMessageElementsButton("reward-button", "REDEEM MEOW", "success")
-        )
-
-        val flexMessageText: List<EmbeddedMessageElementsText> = listOf(
-            EmbeddedMessageElementsText("body", "CATS RULE!!!", "label")
-        )
-
-        val flexMessageElements = EmbeddedMessageElements(
-            "Iterable Coffee Shoppe",
-            "SAVE 15% OFF NOW",
-            "http://placekitten.com/200/300",
-            flexMessageDefaultAction,
-            flexMessageButtons,
-            flexMessageText
-        )
-
-        val payload = JSONObject()
-
-        val flexMessageJson = JSONObject()
-        flexMessageJson.put("metadata", flexMessageMetadata.toJSONObject())
-        flexMessageJson.put("elements", flexMessageElements.toJSONObject())
-        flexMessageJson.put("payload", payload)
+        val file = File("data.json")
+        val bufferedReader = file.bufferedReader()
+        val jsonString = bufferedReader.use { it.readText() }
+        val messageJson = JSONObject(jsonString)
 
         val embeddedMessages = listOf(
-            IterableEmbeddedMessage.fromJSONObject(flexMessageJson),
-            IterableEmbeddedMessage.fromJSONObject(flexMessageJson),
-            IterableEmbeddedMessage.fromJSONObject(flexMessageJson)
+            IterableEmbeddedMessage.fromJSONObject(messageJson),
+            IterableEmbeddedMessage.fromJSONObject(messageJson),
+            IterableEmbeddedMessage.fromJSONObject(messageJson)
         )
 
         return embeddedMessages
