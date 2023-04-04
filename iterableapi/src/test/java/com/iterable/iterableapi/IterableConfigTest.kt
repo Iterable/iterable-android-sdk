a3230ea0a5482a6114e10ad6cc4dd47360d90e57
evan.greer@iterable.com <evan.greer@evan.greer>
2023-04-04 10:08:14 -0600

    adds data center to config and associated unit tests

---
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableConfigTest.kt
@@ -0,0 +1,25 @@
+package com.iterable.iterableapi
+
+import org.hamcrest.Matchers.`is`
+import org.junit.Assert.*
+import org.junit.Test
+
+class IterableConfigTest {
+
+    @Test
+    fun defaultDataRegion() {
+        val configBuilder: IterableConfig.Builder = IterableConfig.Builder()
+        val config: IterableConfig = configBuilder.build()
+        assertThat(config.dataRegion, `is`(IterableDataRegion.US))
+        assertThat(config.dataRegion.endpoint, `is`("https://api.iterable.com/api/"))
+    }
+
+    @Test
+    fun setDataRegionToEU() {
+        val configBuilder: IterableConfig.Builder = IterableConfig.Builder()
+            .setDataRegion(IterableDataRegion.EU)
+        val config: IterableConfig = configBuilder.build()
+        assertThat(config.dataRegion, `is`(IterableDataRegion.EU))
+        assertThat(config.dataRegion.endpoint, `is`("https://api.eu.iterable.com/api/"))
+    }
+}
