37f07c4f78e6ee8a6ded0b73596d9c936c550f4d
“Akshay <ayyanchira.akshay@gmail.com>
2023-05-02 10:54:06 -0700

    Merge pull request #474 from Iterable/jay/MOB-4666-in-apps-config-memory
    [MOB-4666] in apps config memory

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
@@ -499,8 +499,11 @@ public class IterableApi {
         IterableActivityMonitor.getInstance().addCallback(sharedInstance.activityMonitorListener);
 
         if (sharedInstance.inAppManager == null) {
-            sharedInstance.inAppManager = new IterableInAppManager(sharedInstance, sharedInstance.config.inAppHandler,
-                    sharedInstance.config.inAppDisplayInterval);
+            sharedInstance.inAppManager = new IterableInAppManager(
+                    sharedInstance,
+                    sharedInstance.config.inAppHandler,
+                    sharedInstance.config.inAppDisplayInterval,
+                    sharedInstance.config.useInMemoryStorageForInApps);
         }
 
         loadLastSavedConfiguration(context);
