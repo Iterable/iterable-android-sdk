f743478ec2706e67fb00ca251cc28cd2d74d4e8f
evan.greer@iterable.com <evan.greer@evan.greer>
2023-07-31 15:30:31 -0600

    updates embedded test message deserialization

--- iterableapi/src/test/java/com/iterable/iterableapi/EmbeddedTestUtils.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/EmbeddedTestUtils.java
@@ -6,7 +6,12 @@ import org.json.JSONObject;
 public class EmbeddedTestUtils {
     public static IterableEmbeddedMessage getTestEmbeddedMessage() throws Exception {
         JSONObject payload = new JSONObject(IterableTestUtils.getResourceString("embedded_payload_optional_elements_and_custom_payload.json"));
-        JSONArray jsonArray = payload.optJSONArray(IterableConstants.ITERABLE_EMBEDDED_MESSAGE);
-        return IterableEmbeddedMessage.Companion.fromJSONObject(jsonArray.getJSONObject(0));
+        JSONArray jsonArray = payload.optJSONArray(IterableConstants.ITERABLE_EMBEDDED_MESSAGE_PLACEMENTS);
+
+        JSONObject placementJson = jsonArray.optJSONObject(0);
+        IterableEmbeddedPlacement placement = IterableEmbeddedPlacement.Companion.fromJSONObject(placementJson);
+        IterableEmbeddedMessage message = placement.getMessages().get(0);
+
+        return message;
     }
 }
