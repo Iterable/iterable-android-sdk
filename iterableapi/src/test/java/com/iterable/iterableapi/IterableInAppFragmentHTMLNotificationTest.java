6fdb16fb441fcd693a75f677d1a75e2c3c462052
Hani <haniramani@gmail.com>
2023-09-05 16:55:43 +0530

    added more steps

--- iterableapi/src/test/java/com/iterable/iterableapi/IterableInAppFragmentHTMLNotificationTest.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableInAppFragmentHTMLNotificationTest.java
@@ -1,11 +1,20 @@
 package com.iterable.iterableapi;
 
-import static org.mockito.Mockito.*;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.anyBoolean;
+import static org.mockito.Mockito.anyDouble;
+import static org.mockito.Mockito.anyInt;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.when;
 
 import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.Display;
@@ -21,7 +30,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.annotation.Config;
 
@@ -33,13 +41,7 @@ public class IterableInAppFragmentHTMLNotificationTest {
     private Bundle mockBundle;
 
     @Mock
-    private DialogInterface.OnCancelListener mockCancelListener;
-
-    @Mock
     private IterableHelper.IterableUrlCallback mockCallback;
-
-    @Mock
-    private IterableWebView mockWebView;
 
     @Mock
     private IterableApi mockIterableApi;
@@ -56,7 +58,6 @@ public class IterableInAppFragmentHTMLNotificationTest {
     @Mock
     private OrientationEventListener orientationListener;
 
-
     @Mock
     private Context context;
 
@@ -68,7 +69,6 @@ public class IterableInAppFragmentHTMLNotificationTest {
 
     @Before
     public void setUp() {
-        htmlNotification = new IterableInAppFragmentHTMLNotification();
         MockitoAnnotations.initMocks(this);
         mockCallback = mock(IterableHelper.IterableUrlCallback.class);
         IterableApi.sharedInstance = mockIterableApi;
@@ -91,8 +91,13 @@ public class IterableInAppFragmentHTMLNotificationTest {
         boolean callbackOnCancel = true;
         IterableInAppLocation location = IterableInAppLocation.IN_APP;
         String messageId = "Your Message ID";
-        Double backgroundAlpha = 0.5;
+        double backgroundAlpha = 0.5;
         Rect padding = new Rect();
+
+        assertEquals(htmlString, "Your HTML String");
+        assertTrue(callbackOnCancel);
+        assertEquals(messageId, "Your Message ID");
+        assertEquals(backgroundAlpha, 0.5, 0.5);
 
         IterableInAppFragmentHTMLNotification result = IterableInAppFragmentHTMLNotification.createInstance(
                 htmlString, callbackOnCancel, mockCallback, location, messageId, backgroundAlpha, padding);
@@ -102,7 +107,6 @@ public class IterableInAppFragmentHTMLNotificationTest {
 
     @Test
     public void testOnCreate() {
-        // Mock the getArguments() method
         when(mockBundle.getString(eq("HTML_STRING"), eq("Your HTML String"))).thenReturn("Your HTML String");
         when(mockBundle.getBoolean(eq("CALLBACK_ON_CANCEL"), anyBoolean())).thenReturn(true);
         when(mockBundle.getString(eq("MESSAGE_ID"), eq("Your Message ID"))).thenReturn("Your Message ID");
@@ -113,8 +117,6 @@ public class IterableInAppFragmentHTMLNotificationTest {
         when(mockBundle.getBoolean(eq("IN_APP_SHOULD_ANIMATE"), anyBoolean())).thenReturn(true);
 
         htmlNotification.onCreate(mockBundle);
-
-        // Add assertions as needed to check the behavior of the method.
     }
 
     @Test
