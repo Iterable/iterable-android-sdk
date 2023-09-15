21a5abc83443af8648415b9785697a3145afebaa
evan.greer@iterable.com <evan.greer@evan.greer>
2023-09-14 14:39:57 -1000

    updates view to take in and render message

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import com.iterable.iterableapi.IterableEmbeddedMessage
 import com.iterable.iterableapi.ui.R
 
-class IterableEmbeddedView(style: String): Fragment() {
+class IterableEmbeddedView(style: String, message: IterableEmbeddedMessage): Fragment() {
     var style = style
-//    var message = message
+    var message = message
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,22 +26,22 @@ class IterableEmbeddedView(style: String): Fragment() {
             else -> inflater.inflate(R.layout.banner_view, container, false)
         }
 
-//        bind(rootView, message)
+        bind(rootView, message)
 
         return rootView
     }
 
-//    fun bind(view: View, message: IterableEmbeddedMessage): View  {
-//        var embeddedMessageViewTitle: TextView = view.findViewById(R.id.embedded_message_title)
-//        var embeddedMessageViewBody: TextView = view.findViewById(R.id.embedded_message_body)
-//        var embeddedMessageViewButton1: TextView? = view.findViewById(R.id.embedded_message_first_button)
-//        var embeddedMessageViewButton2: TextView? = view.findViewById(R.id.embedded_message_second_button)
-//
-//        embeddedMessageViewTitle.text = message.elements?.title
-//        embeddedMessageViewBody.text = message.elements?.body
-//        embeddedMessageViewButton1?.text = message.elements?.buttons?.get(0)?.title
-//        embeddedMessageViewButton2?.text = message.elements?.buttons?.get(1)?.title
-//
-//        return view
-//    }
+    fun bind(view: View, message: IterableEmbeddedMessage): View  {
+        var embeddedMessageViewTitle: TextView = view.findViewById(R.id.embedded_message_title)
+        var embeddedMessageViewBody: TextView = view.findViewById(R.id.embedded_message_body)
+        var embeddedMessageViewButton1: TextView? = view.findViewById(R.id.embedded_message_first_button)
+        var embeddedMessageViewButton2: TextView? = view.findViewById(R.id.embedded_message_second_button)
+
+        embeddedMessageViewTitle.text = message.elements?.title
+        embeddedMessageViewBody.text = message.elements?.body
+        embeddedMessageViewButton1?.text = message.elements?.buttons?.get(0)?.title
+        embeddedMessageViewButton2?.text = message.elements?.buttons?.get(1)?.title
+
+        return view
+    }
 }
