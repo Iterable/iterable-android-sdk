6c2e5f8c747d7923f17362d21610b1b0197c3ec6
Evan Greer <evan.greer@evan.greer>
2023-10-11 15:33:57 -0600

    stashed changes

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableEmbeddedManager.kt
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableEmbeddedManager.kt
@@ -201,6 +201,7 @@ public class IterableEmbeddedManager : IterableActivityMonitor.AppStateCallback 
     override fun onSwitchToForeground() {
         IterableLogger.printInfo()
         embeddedSessionManager.startSession()
+        IterableLogger.d(TAG, "Calling start session")
         syncMessages()
     }
 
