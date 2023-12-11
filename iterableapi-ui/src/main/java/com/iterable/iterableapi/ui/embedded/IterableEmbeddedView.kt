4bdfbf9e90c200e16690be8f100a204ec35b3abb
Evan Greer <evan.greer@evan.greer>
2023-12-11 16:17:19 -0700

    adds image processing

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
@@ -1,18 +1,18 @@
 package com.iterable.iterableapi.ui.embedded
 
-import android.graphics.Color
 import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.iterable.iterableapi.IterableApi
 import com.iterable.iterableapi.IterableEmbeddedMessage
-import com.iterable.iterableapi.IterableLogger
 import com.iterable.iterableapi.ui.R
 
 class IterableEmbeddedView(
@@ -146,6 +146,8 @@ class IterableEmbeddedView(
         val embeddedMessageViewButton: TextView = view.findViewById(R.id.embedded_message_first_button)
         val embeddedMessageViewButton2: TextView = view.findViewById(R.id.embedded_message_second_button)
 
+        val embeddedMessageImageView: ImageView = view.findViewById(R.id.embedded_message_image)
+
         embeddedMessageViewTitle.text = message.elements?.title
         embeddedMessageViewBody.text = message.elements?.body
 
@@ -195,6 +197,10 @@ class IterableEmbeddedView(
             embeddedMessageViewButton.visibility = View.GONE
             embeddedMessageViewButton2.visibility = View.GONE
         }
+
+        Glide.with(view.context)
+                .load(message.elements?.mediaURL)
+                .into(embeddedMessageImageView)
 
         return view
     }
