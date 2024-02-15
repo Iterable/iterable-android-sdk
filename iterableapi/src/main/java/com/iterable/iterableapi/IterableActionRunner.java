551246cecfdc6480913c9159960eeb9306c06b88
hani <hani.vora@iterable.com>
2024-02-15 17:56:48 +0530

    resolve codeclimate error

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableActionRunner.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableActionRunner.java
@@ -58,6 +58,7 @@ class IterableActionRunner {
          * `false` if the handler did not handle this URL and no activity was found to open it with
          */
         private boolean openUri(@NonNull Context context, @NonNull Uri uri, @NonNull IterableActionContext actionContext) {
+            boolean uriHandled = false;
             // Handle URL: check for deep links within the app
             if (!IterableUtil.isUrlOpenAllowed(uri.toString())) {
                 return false;
@@ -93,11 +94,11 @@ class IterableActionRunner {
 
             if (intent.resolveActivity(context.getPackageManager()) != null) {
                 context.startActivity(intent);
-                return true;
+                uriHandled = true;
             } else {
                 IterableLogger.e(TAG, "Could not find activities to handle deep link:" + uri);
-                return false;
             }
+            return uriHandled;
         }
 
         /**
