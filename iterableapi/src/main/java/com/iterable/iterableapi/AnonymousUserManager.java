820e1ef4e2a9373276c7c38d3c3c98c907ad2219
Evan Greer <evan.greer@evan.greer>
2024-10-18 09:22:02 -0600

    removes unused import

--- iterableapi/src/main/java/com/iterable/iterableapi/AnonymousUserManager.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/AnonymousUserManager.java
@@ -3,7 +3,6 @@ package com.iterable.iterableapi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.pm.ApplicationInfo;
-import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
