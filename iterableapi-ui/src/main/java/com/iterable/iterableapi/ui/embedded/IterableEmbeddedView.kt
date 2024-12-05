d7acbbda0e47825666f7d756061b95ff0e41601e
Evan Greer <evan.greer@evan.greer>
2024-12-05 12:15:19 -1000

    removes unused import

--- iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
+++ iterableapi-ui/src/main/java/com/iterable/iterableapi/ui/embedded/IterableEmbeddedView.kt
@@ -6,7 +6,6 @@ import android.os.Build
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.LayoutInflater
-import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
