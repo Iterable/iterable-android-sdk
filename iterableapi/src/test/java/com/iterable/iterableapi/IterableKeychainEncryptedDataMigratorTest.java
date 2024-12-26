05e22851485e0402a5a4c32a576a3d1c50914a55
Sumeru Chatterjee <sumeru.chatterjee@iterable.com>
2024-12-26 03:43:16 +0100

    [MOB-9235] Cleanup

--- iterableapi/src/test/java/com/iterable/iterableapi/IterableKeychainEncryptedDataMigratorTest.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableKeychainEncryptedDataMigratorTest.java
@@ -11,11 +11,9 @@ import org.mockito.MockitoAnnotations;
 import org.robolectric.util.ReflectionHelpers;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
