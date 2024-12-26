5cbcd5c54eb4fb02d5b1506840610d3ab5ce75c6
Sumeru Chatterjee <sumeru.chatterjee@iterable.com>
2024-12-26 03:52:06 +0100

    [MOB-9235] Cleanup

--- iterableapi/src/test/java/com/iterable/iterableapi/IterableDataEncryptorTest.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableDataEncryptorTest.java
@@ -12,6 +12,8 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
 
 import java.security.KeyStore;
 import java.util.ArrayList;
--- iterableapi/src/test/java/com/iterable/iterableapi/IterableKeychainEncryptedDataMigratorTest.java
+++ iterableapi/src/test/java/com/iterable/iterableapi/IterableKeychainEncryptedDataMigratorTest.java
@@ -14,12 +14,19 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 import static org.mockito.Mockito.verify;
 
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicReference;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.anyBoolean;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
 
 public class IterableKeychainEncryptedDataMigratorTest extends BaseTest {
 
