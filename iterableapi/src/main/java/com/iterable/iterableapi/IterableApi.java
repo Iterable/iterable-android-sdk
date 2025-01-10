9e04f804c88577e546d8a5ad6a44d2aebebbb7c1
Sumeru Chatterjee <sumeru.chatterjee@iterable.com>
2025-01-10 12:45:46 +0100

    [MOB-9446] Logic fix

--- iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
+++ iterableapi/src/main/java/com/iterable/iterableapi/IterableApi.java
@@ -392,25 +392,25 @@ public class IterableApi {
     private void onForeground() {
         if (!_firstForegroundHandled) {
             _firstForegroundHandled = true;
-			if (sharedInstance.config.autoPushRegistration && sharedInstance.isInitialized()) {
-				sharedInstance.registerForPush();
-			}
-			fetchRemoteConfiguration();
-		}
+            if (sharedInstance.config.autoPushRegistration && sharedInstance.isInitialized()) {
+                sharedInstance.registerForPush();
+            }
+            fetchRemoteConfiguration();
+        }
 
-		boolean systemNotificationEnabled = NotificationManagerCompat.from(_applicationContext).areNotificationsEnabled();
-		SharedPreferences sharedPref = sharedInstance.getMainActivityContext().getSharedPreferences(IterableConstants.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
-		boolean isNotificationEnabled = sharedPref.getBoolean(IterableConstants.SHARED_PREFS_DEVICE_NOTIFICATIONS_ENABLED, false);
+        boolean systemNotificationEnabled = NotificationManagerCompat.from(_applicationContext).areNotificationsEnabled();
+        SharedPreferences sharedPref = sharedInstance.getMainActivityContext().getSharedPreferences(IterableConstants.SHARED_PREFS_FILE, Context.MODE_PRIVATE);
+        boolean isNotificationEnabled = sharedPref.getBoolean(IterableConstants.SHARED_PREFS_DEVICE_NOTIFICATIONS_ENABLED, false);
 
-		if (sharedInstance.isInitialized() && isNotificationEnabled != systemNotificationEnabled) {
-			if (!systemNotificationEnabled) {
-				sharedInstance.disablePush();
-			}
+        if (sharedInstance.isInitialized() && isNotificationEnabled != systemNotificationEnabled) {
+            if (!systemNotificationEnabled) {
+                sharedInstance.disablePush();
+            }
 
-			SharedPreferences.Editor editor = sharedPref.edit();
-			editor.putBoolean(IterableConstants.SHARED_PREFS_DEVICE_NOTIFICATIONS_ENABLED, systemNotificationEnabled);
-			editor.apply();
-		}
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putBoolean(IterableConstants.SHARED_PREFS_DEVICE_NOTIFICATIONS_ENABLED, systemNotificationEnabled);
+            editor.apply();
+        }
     }
 
     private boolean isInitialized() {
