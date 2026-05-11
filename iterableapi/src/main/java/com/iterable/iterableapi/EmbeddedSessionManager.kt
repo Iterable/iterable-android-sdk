b118df482f4a9cb19bea50099e86b1eded79dddf
Mahmoud Elshamy <shamyyoun@gmail.com>
2026-05-11 19:16:19 +0400

    Fix NPE race condition in updateDisplayCountAndDuration

--- iterableapi/src/main/java/com/iterable/iterableapi/EmbeddedSessionManager.kt
+++ iterableapi/src/main/java/com/iterable/iterableapi/EmbeddedSessionManager.kt
@@ -108,10 +108,11 @@ public class EmbeddedSessionManager {
     }
 
     private fun updateDisplayCountAndDuration(impressionData: EmbeddedImpressionData): EmbeddedImpressionData {
-        if (impressionData.start != null) {
+        val start = impressionData.start
+        if (start != null) {
             impressionData.displayCount = impressionData.displayCount.plus(1)
             impressionData.duration =
-                impressionData.duration.plus((Date().time - impressionData.start!!.time) / 1000.0)
+                impressionData.duration.plus((Date().time - start.time) / 1000.0)
                     .toFloat()
             impressionData.start = null
         }
